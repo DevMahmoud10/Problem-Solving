db740473107c90eaa8d472f11cefb44b7963e3c7
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-05-08 18:48:05 +0300

    Time: 73 ms (34.48%), Space: 17.6 MB (71.46%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,13 +1,25 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        ranked_score=sorted(score,reverse=True)
-        nums_dict={n:i+1 for i,n in enumerate(ranked_score)}
-        for i,n in enumerate(score):
-            score[i]=str(nums_dict[n])
-            if score[i]=='1':
-                score[i]='Gold Medal'
-            elif score[i]=='2':
-                score[i]='Silver Medal'
-            elif score[i]=='3':
-                score[i]='Bronze Medal'
-        return score
+        n = len(score)
+        maxScore = max(score)
+        
+        buckets = [0]*(maxScore+1)
+        for i in range(n):
+            buckets[score[i]] = i+1
+            
+        res = ['']*n
+        rank = 1
+        for i in range(maxScore, -1, -1):
+            if buckets[i] == 0:
+                continue
+            actual_index = buckets[i]-1
+            if rank == 1:
+                res[actual_index] = "Gold Medal"
+            elif rank == 2:
+                res[actual_index] = "Silver Medal"
+            elif rank == 3:
+                res[actual_index] = "Bronze Medal"
+            else:
+                res[actual_index] = str(rank)
+            rank += 1
+        return res
