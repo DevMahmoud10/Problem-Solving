78c148601cf8095a5765a5217b1f626f74556b9a
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-05-14 13:38:32 +0300

    Time: 3362 ms (33.57%), Space: 16.8 MB (10.89%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,13 +1,14 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        def dfs(r,c,visited):
-            if 0<=r<R and 0<=c<C and grid[r][c]!=0 and (r,c) not in visited:
-                visited.add((r,c))
-                total=grid[r][c]
+        def dfs(r,c):
+            if 0<=r<R and 0<=c<C and grid[r][c]!=0:
+                val=grid[r][c]
+                grid[r][c]=0
+                total=val
                 dirs=[(1,0),(-1,0),(0,1),(0,-1)]
                 for dr,dc in dirs:
-                    total=max(total, grid[r][c]+dfs(r+dr,c+dc,visited))
-                visited.remove((r,c))
+                    total=max(total, val+dfs(r+dr,c+dc))
+                grid[r][c]=val
                 return total
             return 0
         
@@ -15,5 +16,5 @@ class Solution:
         res=0
         for i in range(R):
             for j in range(C):
-                res=max(res, dfs(i,j,set()))
+                res=max(res, dfs(i,j))
         return res
