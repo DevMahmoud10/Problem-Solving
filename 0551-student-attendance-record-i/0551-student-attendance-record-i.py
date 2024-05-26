0662a0a931c9c1fdb21cbd6a8cd98c8b8f3d993b
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-05-26 15:58:04 +0300

    Time: 35 ms (54.53%), Space: 16.6 MB (56.06%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,13 +1,3 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        if s.count('A')>=2:
-            return False
-        late_days=0
-        for day in s:
-            if day=='L':
-                late_days+=1
-            else:
-                late_days=0
-            if late_days==3:
-                return False
-        return True
+        return s.count('A')<2 and 'LLL' not in s
