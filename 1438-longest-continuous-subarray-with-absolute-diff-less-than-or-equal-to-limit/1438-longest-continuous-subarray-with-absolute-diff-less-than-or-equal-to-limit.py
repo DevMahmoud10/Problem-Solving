0f6097b361dfef02f10a1bead7282f88f6b4a246
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-06-23 21:55:00 +0300

    Time: 447 ms (62.40%), Space: 25.7 MB (93.67%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,25 +1,25 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        res=0
+        max_deque = deque()
+        min_deque = deque()
+        res = 0
         l=0
-        min_heap,max_heap=[],[]
         
         for r in range(len(nums)):
-            heapq.heappush(min_heap, (nums[r],r))
-            heapq.heappush(max_heap, (-nums[r],r))
-            
-            while -max_heap[0][0]-min_heap[0][0]>limit:
-                l=1+min(max_heap[0][1], min_heap[0][1])
-                
-                while max_heap[0][1]<l:
-                    heapq.heappop(max_heap)
-                while min_heap[0][1]<l:
-                    heapq.heappop(min_heap)
-            
-            res=max(res, r-l+1)
+            while max_deque and max_deque[-1] < nums[r]:
+                max_deque.pop()
+            max_deque.append(nums[r])
+
+            while min_deque and min_deque[-1] > nums[r]:
+                min_deque.pop()
+            min_deque.append(nums[r])
+
+            while max_deque[0] - min_deque[0] > limit:
+                if max_deque[0] == nums[l]:
+                    max_deque.popleft()
+                if min_deque[0] == nums[l]:
+                    min_deque.popleft()
+                l+=1
+            res = max(res, r-l+1)
             
         return res
-            
-            
-                
-                
