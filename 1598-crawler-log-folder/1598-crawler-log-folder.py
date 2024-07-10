80ca242bdf927b9bdf08258e11ce7304ca34d1fa
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-07-10 13:11:35 +0300

    Time: 43 ms (89.88%), Space: 16.7 MB (82.14%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth=0
+        for log in logs:
+            if log=='./':
+                continue
+            elif log=='../':
+                depth=max(depth-1, 0)
+            else:
+                depth+=1
+        return depth
+                
