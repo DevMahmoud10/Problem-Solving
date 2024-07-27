b89229ef4c635c77406e46321271e8f413df9717
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-07-27 14:55:03 +0300

    Time: 42 ms (36.22%), Space: 16.5 MB (13.12%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,15 +1,16 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        res=0
         buckets=[0]*(max(heights)+1)
         for h in heights:
             buckets[h]+=1
+        
         expected=[]
         for n,c in enumerate(buckets):
-            for _ in range(c):
-                expected.append(n)
-        for i in range(len(heights)):
-            if expected[i]!=heights[i]:
+            expected.extend([n]*c)
+        
+        res=0
+        for h,e in zip(heights,expected):
+            if h!=e:
                 res+=1
         return res
         
