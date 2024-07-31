9bf6cac10eef91faf832b581d8d0f31b48426707
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2024-07-31 13:51:12 +0300

    Time: 38 ms (35.15%), Space: 16.4 MB (90.66%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,9 +1,3 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        max_a,min_b=-1,-1
-        for i in range(len(s)):
-            if s[i]=='a':
-                max_a=i
-            elif s[i]=='b' and min_b==-1:
-                min_b=i
-        return True if (max_a<0 or min_b<0) else max_a<min_b
+        return 'ba' not in s
